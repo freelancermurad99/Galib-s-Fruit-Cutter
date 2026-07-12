779fbb2045cfe05b1ae1593ba3f9ea86f361e8fa
Md. Asadullah Hil Galib <freelancermurad99@gmail.com>
2026-07-13 00:31:59 +0600

    for fb instant games upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,28 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+declare global {
+  interface Window {
+    FBInstant: any;
+  }
+}
+
+const renderApp = () => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+if (typeof window.FBInstant !== "undefined") {
+  window.FBInstant.initializeAsync().then(() => {
+    window.FBInstant.setLoadingProgress(100);
+    window.FBInstant.startGameAsync().then(() => {
+      renderApp();
+    });
+  });
+} else {
+  // Run normally if not inside Facebook Instant Games
+  renderApp();
+}
